1b81e84f26690427f5e2f725fedf48f858f18417
Doreen Hakimi <doreen.hakimi@gmail.com>
2018-03-20 13:19:34 -0700

    yay link gets added

--- app/components/Header.js
+++ app/components/Header.js
@@ -1,28 +1,40 @@
 import React, { PropTypes, Component } from 'react';
 import TodoTextInput from './TodoTextInput';
 
+var Promise = require('bluebird')
+
 export default class Header extends Component {
 
   static propTypes = {
     addTodo: PropTypes.func.isRequired
   };
 
-  handleSave = (text) => {
-    if (text.length !== 0) {
-      this.props.addTodo(text);
-    }
+  handleSave = () => {
+    console.log('handle save')
+    chrome.tabs.getSelected(null, tab => {
+      chrome.extension.getBackgroundPage().console.log(tab.url);
+      this.props.addTodo(tab.url);
+    })
   };
+
+  componentDidMount() {
+    chrome.extension.getBackgroundPage().console.log('loaded');
+  }
 
   render() {
     return (
-      <header>
-        <h1>todos</h1>
-        <TodoTextInput
-          newTodo
-          onSave={this.handleSave}
-          placeholder="What needs to be done?"
-        />
-      </header>
-    );
+      <div onClick={this.handleSave}>+ Add Current Page</div>
+    )
   }
 }
+
+//    return (
+//      <header>
+//        <h1>todos</h1>
+//        <TodoTextInput
+//          newTodo
+//          onSave={this.handleSave}
+//          placeholder="What needs to be done?"
+//        />
+//      </header>
+//    );
