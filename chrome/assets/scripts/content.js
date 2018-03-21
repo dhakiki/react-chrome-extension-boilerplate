9b10bcf8a43930923aa90e7d4717a81244d046a7
Doreen Hakimi <doreen.hakimi@gmail.com>
2018-03-21 15:59:17 -0700

    got the xpath generation and reversal in too

--- chrome/assets/scripts/content.js
+++ chrome/assets/scripts/content.js
@@ -1,4 +1,45 @@
+//dom node > xpath function captured from: https://stackoverflow.com/a/30227178
+function getPathTo(element) {
+    if (element.tagName == 'HTML')
+        return '/HTML[1]';
+    if (element===document.body)
+        return '/HTML[1]/BODY[1]';
+
+    var ix= 0;
+    var siblings= element.parentNode.childNodes;
+    for (var i= 0; i<siblings.length; i++) {
+        var sibling= siblings[i];
+        if (sibling===element)
+            return getPathTo(element.parentNode)+'/'+element.tagName+'['+(ix+1)+']';
+        if (sibling.nodeType===1 && sibling.tagName===element.tagName)
+            ix++;
+    }
+}
+//xpath > dom node plugin function captured from https://stackoverflow.com/a/20495940
+$.fn.xpathEvaluate = function (xpathExpression) {
+   // NOTE: vars not declared local for debug purposes
+   $this = this.first(); // Don't make me deal with multiples before coffee
+
+   // Evaluate xpath and retrieve matching nodes
+   xpathResult = this[0].evaluate(xpathExpression, this[0], null, XPathResult.ORDERED_NODE_ITERATOR_TYPE, null);
+
+   result = [];
+   while (elem = xpathResult.iterateNext()) {
+      result.push(elem);
+   }
+
+   $result = jQuery([]).pushStack( result );
+   return $result;
+}
+
 $( document ).ready(function() {
+  chrome.runtime.onMessage.addListener(
+    function(requrest, sender, sendResponse) {
+      if (sender.tab) sendResponse({complete: "true"})
+      if (request.type == "activateSelectionMode") {
+        console.log('activate selection mode');
+      }
+    });
   //TODO: consult what we should accept
   const acceptableElements = "h1, h2, h3, h4, h5, h6, div, span, label, p";
   $(acceptableElements).hover(
@@ -21,7 +62,10 @@ $( document ).ready(function() {
       .trim();
     if ($(this).find("*").length <= 1) {
       if (val.length > 0) {
-        console.log('dude i should totally log this', val);
+        $(this).removeClass('hover');
+        console.log('dude i should totally log this', this, val);
+        console.log(getPathTo(this));
+        console.log($(document).xpathEvaluate(getPathTo(this)).get(0));
       }
       e.stopPropagation();
     }
